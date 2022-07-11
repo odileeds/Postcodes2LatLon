2bacfd14df5af9fe9da7e84d6f68567453c9cd66
slowe <299787+slowe@users.noreply.github.com>
2022-07-11 12:44:13 +0100

    Update postcodes to May 2022

--- postcodes/postcodesplitter.pl
+++ postcodes/postcodesplitter.pl
@@ -1,49 +1,55 @@
 #!/usr/bin/perl
 # Split the Postcode lookup into a single geography
 
+use Data::Dumper;
+
 @categories = @ARGV;
 if(!@categories){ @categories = ("lat","long"); }
 
-$file = "National_Statistics_Postcode_Lookup_Latest_Centroids.csv";
+$file = "NSPL_MAY_2022_UK.csv";
 
 if(!-e $file){
-	print "The National Statistics Postcode Lookup doesn't seem to exist. Please download a copy from:\n";
-	print "http://geoportal.statistics.gov.uk/datasets/national-statistics-postcode-lookup-latest-centroids\n";
+	print "The National Statistics Postcode Lookup doesn't seem to exist. Please download a copy from e.g.:\n";
+	print "https://geoportal.statistics.gov.uk/datasets/national-statistics-postcode-lookup-may-2022/\n";
 	print "And save it as $file\n";
 	exit;
 }
 
-open(FILE,"National_Statistics_Postcode_Lookup_Latest_Centroids.csv");
-#X,Y,objectid,pcd,pcd2,pcds,dointr,doterm,usertype,oseast1m,osnrth1m,osgrdind,oa11,cty,laua,ward,hlthau,ctry,pcon,eer,teclec,ttwa,pct,nuts,park,lsoa11,msoa11,wz11,ccg,bua11,buasd11,ru11ind,oac11,lat,long,lep1,lep2,pfa,imd,ced,nhser,rgn,calncv,stp
 $pcd = -1;
 @cat;
 for($c = 0; $c < @categories; $c++){ $cat[$c] = -1; }
 
+#X,Y,objectid,pcd,pcd2,pcds,dointr,doterm,usertype,oseast1m,osnrth1m,osgrdind,oa11,cty,laua,ward,hlthau,ctry,pcon,eer,teclec,ttwa,pct,nuts,park,lsoa11,msoa11,wz11,ccg,bua11,buasd11,ru11ind,oac11,lat,long,lep1,lep2,pfa,imd,ced,nhser,rgn,calncv,stp
+#pcd,pcd2,pcds,dointr,doterm,usertype,oseast1m,osnrth1m,osgrdind,oa11,cty,ced,laua,ward,hlthau,nhser,ctry,rgn,pcon,eer,teclec,ttwa,pct,itl,park,lsoa11,msoa11,wz11,ccg,bua11,buasd11,ru11ind,oac11,lat,long,lep1,lep2,pfa,imd,calncv,stp
 open(FILE,$file);
 $counter = 0;
 %pcs;
 
 # Regex for postcodes ([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})
-
+%headerlookup;
 while(my $line = <FILE>){
 
+	$line =~ s/\"//g;
 	(@cols) = split(/,/,$line);
 	
 	if(!$header){
 		$header = $line;
 		@headers = @cols;
 		for($i = 0; $i < @headers; $i++){
+			$headerlookup{$headers[$i]} = $i;
 			if($headers[$i] eq "pcd"){ $pcd = $i; }
 			for($c = 0; $c < @categories; $c++){
 				if($headers[$i] eq $categories[$c]){ $cat[$c] = $i; }
 			}
 		}
+
 	}else{
 	
-		$cols[3] =~ s/ +/ /g;
-		if($cols[3] =~ /^([A-Z]{1,2})[0-9]/){
+		$cols[$headerlookup{'pcd'}] =~ s/ +/ /g;
+		if($cols[$headerlookup{'pcd'}] =~ /^([A-Z]{1,2})[0-9]/){
 			$bit = $1;
 		}
+
 		if($bit){
 			if(!$pcs{$bit}){
 				$pcs{$bit} = 0;
@@ -65,7 +71,7 @@ while(my $line = <FILE>){
 				}
 			}
 
-			print CSV $cols[3];
+			print CSV $cols[$headerlookup{'pcd'}];
 			
 			for($c = 0; $c < @categories; $c++){
 				print CSV ",";
